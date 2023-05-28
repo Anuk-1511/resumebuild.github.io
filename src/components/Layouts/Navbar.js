b17bb86eb7dce23060f718ea9f20b9e1c4652819
Anubhav Kharwar <61133543+Anuk-1511@users.noreply.github.com>
2023-05-28 16:01:24 +0530

    Update Navbar.js

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -1,38 +1,59 @@
 import { IconButton } from '@chakra-ui/button';
 import {
-    Stack,
-    Flex,
-    Heading,
-    Spacer,
-    HStack,
-    Text,
-    Button,
+  Stack,
+  Flex,
+  Heading,
+  Spacer,
+  HStack,
+  Text,
+  Button,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Navbar = () => {
-    return (
-        <Stack
-            p={5}
-            bg={'gray.50'}
-            as='header'
+  const [isMobile] = useMediaQuery("(max-width: 600px)");
+
+  return (
+    <Stack p={5} bg={'gray.50'} as='header'>
+      <Flex w='full' alignItems={'center'}>
+        <Heading
+          as='h3'
+          ml={{ base: 0, sm: 8 }}
+          size={isMobile ? 'md' : 'lg'}
+          fontWeight={'thin'}
+          color='green.500'
+          fontFamily="Pacifico"
         >
-            <Flex
-                w='full'
-                alignItems={'center'}
-            >
-                <Heading as='h3' ml={{ base: 0, sm: 8 }} size='lg' fontWeight={'thin'} color='green.500' style={{ fontFamily: "Pacifico" }}>ResumeBuild</Heading>
-                <Spacer></Spacer>
-                <HStack spacing={10} mr={{ base: 0, sm: 8 }} as='nav' style={{ fontFamily: 'Poppins' }}>
-                    <Text as='a' href='#' fontSize='lg'>Home</Text>
-                    <Text as='a' href='#' fontSize='lg'>Templates</Text>
-                    <Text as='a' href='#' fontSize='lg'>About</Text>
-                    <Button colorScheme={'green'} fontWeight={'medium'}>Contact</Button>
-                </HStack>
-            </Flex>
+          ResumeBuild
+        </Heading>
+        <Spacer />
+        {isMobile ? (
+          <IconButton
+            aria-label='Toggle navigation'
+            icon={<FaInstagram />}
+            mr={{ base: 0, sm: 8 }}
+            variant='ghost'
+          />
+        ) : (
+          <HStack spacing={10} mr={{ base: 0, sm: 8 }} as='nav' fontFamily='Poppins'>
+            <Text as='a' href='#' fontSize='lg'>
+              Home
+            </Text>
+            <Text as='a' href='#' fontSize='lg'>
+              Templates
+            </Text>
+            <Text as='a' href='#' fontSize='lg'>
+              About
+            </Text>
+            <Button colorScheme={'green'} fontWeight={'medium'}>
+              Contact
+            </Button>
+          </HStack>
+        )}
+      </Flex>
+    </Stack>
+  );
+};
 
-        </Stack>
-    )
-}
-
-export default Navbar
+export default Navbar;
